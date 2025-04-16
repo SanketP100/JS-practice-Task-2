ef1116d7a35a266471fd00cfc9eb6173b9f807c3
SanketP100 <sanketpowar100@gmail.com>
2025-04-16 18:32:27 +0530

    js code update

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ var btns = document.querySelector("#buttons")
 
 var posX = 175;
 var posY = 175;
-btns.addEventListener("click", function (dtl) {
 
-    console.log(dtl);
-
-
+btns.addEventListener("click", dtl => {
+    
     if (dtl.target.id === "up") {
         console.log("up");
         posY -= 30
@@ -38,5 +36,95 @@ btns.addEventListener("click", function (dtl) {
         img.setAttribute('src', "./imgs/back.gif")
     }
 
-
 })
+
+document.addEventListener('keyup',keydtl => {
+   
+    if (keydtl.key == 'ArrowUp') {
+        console.log("up");
+        posY -= 30
+        imgbox.style.top = posY + "px"
+        img.setAttribute('src', "./imgs/topWalk.gif")
+        
+    }else if (keydtl.key == 'ArrowDown') {
+        console.log("down");
+        posY += 30
+        imgbox.style.top = posY + "px"
+        img.setAttribute('src', "./imgs/backWalk.gif")
+        
+    }
+    else if (keydtl.key == 'ArrowRight') { 
+        console.log("right");
+        posX += 150
+        imgbox.style.left = posX + "px"
+        img.setAttribute('src', "./imgs/leftrun.gif")
+        
+    }else if (keydtl.key == 'ArrowLeft') {
+        console.log("left");
+        posX -= 30
+        imgbox.style.left = posX + "px"
+        img.setAttribute('src', "./imgs/leftWalk.gif")
+         
+    }
+    else {
+        img.setAttribute('src', "./imgs/back.gif")
+    }
+    
+})
+
+
+
+
+
+
+
+// chatGpt kadun smjun ghetlel Logic
+
+// function moveCharacter(dtl) {
+
+//     if (dtl === "up") {
+//         console.log("up");
+//         posY -= 30;
+//         imgbox.style.top = posY + "px";
+//         img.setAttribute('src', "./imgs/topWalk.gif");
+
+//     } else if (dtl === "down") {
+//         console.log("down");
+//         posY += 30;
+//         imgbox.style.top = posY + "px";
+//         img.setAttribute('src', "./imgs/backWalk.gif");
+
+//     } else if (dtl === "right") {
+//         console.log("right");
+//         posX += 150;
+//         imgbox.style.left = posX + "px";
+//         img.setAttribute('src', "./imgs/leftrun.gif");
+
+//     } else if (dtl === "left") {
+//         console.log("left");
+//         posX -= 30;
+//         imgbox.style.left = posX + "px";
+//         img.setAttribute('src', "./imgs/leftWalk.gif");
+
+//     } else {
+//         img.setAttribute('src', "./imgs/back.gif");
+//     }
+// }
+
+// // Button click handler
+// btns.addEventListener("click", function (dtl) {
+//     moveCharacter(dtl.target.id);
+// });
+
+// // Keyboard arrow key handler
+// document.addEventListener("keyup", function (keydtl) {
+   
+//     const keyMap = {
+//         "ArrowUp": "up",
+//         "ArrowDown": "down",
+//         "ArrowRight": "right",
+//         "ArrowLeft": "left"
+//     };
+
+//     moveCharacter(keyMap[keydtl.key] || "");
+// });
